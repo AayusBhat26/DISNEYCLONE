256f71519d68d71a78e8f8ff77ed18b7ef47bd6e
Aayush Kumar Bhat <kumarbhataayush@gmail.com>
2022-03-27 09:44:35 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,98 @@
+const InnerContainer = document.querySelector('.InnerContainer');
+
+let sliders = [];
+let index = 0;
+
+const createSlide = () => {
+    if(index >= movies.length){
+        index = 0;
+    }
+    // we are creating elements here
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+    
+    // let h1Name = document.createElement('h1');
+
+    imgElement.appendChild(document.createTextNode(''));
+    
+    h1.appendChild(document.createTextNode(movies[index].Name));
+    // h1Name.appendChild(document.createTextNode(movies[index].Name))
+    p.appendChild(document.createTextNode(movies[index].des));
+    
+    content.appendChild(h1); 
+    // content.append(h1Name);
+    content.appendChild(p);
+
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    
+    InnerContainer.appendChild(slide);
+
+    imgElement.src = movies[index].image;
+    index++;
+    slide.className = 'slider';
+    content.className = 'slideContent';
+    h1.className = 'Name';
+    // h1Name.className = 'Name';
+    p.className = 'des';
+
+    sliders.push(slide);
+
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2 )}% -   ${30 * (sliders.length - 2)}px)`;
+        // sliders[0].style.marginRight = `50px`;
+    }
+    // here calc is a css formula for calculation
+    // - (negative sign) indicates for negative margin left
+    // multiplying with 100 as we want negative 100% per slide.
+    // subtracting 2 from slider.length as we want 2nd slide to be in the middle.
+    //  ${30 * (sliders.length - 2)} same calculation as of left side, here it is used for 30px margin right
+
+}
+// // function fun(){
+
+
+// // }
+    for(let i = 0; i < 7; i++){
+        createSlide();
+    }
+setInterval(() =>{
+    createSlide();
+},3500)
+
+
+// card -> videos
+const videoCards = [...document.querySelectorAll('.videoCard')]
+// yaha per dot use hoga kyuki hum class ko select kr rhe hai. (bhul mat jana)
+videoCards.forEach(item =>{
+    item.addEventListener('mouseover', () =>{
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () =>{
+        let video = item.children[1];
+        video.pause();
+    })
+})
+// card sliders
+let cardContainers = [...document.querySelectorAll('.cardContainer')];
+let preBtn = [...document.querySelectorAll('.preBtn')];
+let nextBtn = [...document.querySelectorAll('.nextBtn')];
+
+cardContainers.forEach((item,i) => {
+    let dimContainer = item.getBoundingClientRect();
+    // this will return the dimensions and the position of the element 
+    let widthContainer = dimContainer.width;
+
+    nextBtn[i].addEventListener('click', () =>{
+        item.scrollLeft += widthContainer - 100;
+
+    })
+    preBtn[i].addEventListener('click', () =>{
+        item.scrollLeft -= widthContainer + 100;
+
+    })
+})
---
+++ data.js
@@ -0,0 +1,47 @@
+let movies = [
+    {
+        Name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        Name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        Name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        Name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        Name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    },
+    {
+        Name: 'random',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 6.webp'
+    },
+    {
+        Name: 'A Thursday',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 7.webp'
+    },
+    {
+        Name: 'random',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 8.webp'
+    },
+    {
+        Name: '83',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 9.webp'
+    }
+]
